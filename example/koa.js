dfab2ef09fa126a8ad02027e92c680040acd113d
Duarte David <deltaduartedavid@gmail.com>
2020-04-19 21:26:06 +0200

    remove useless graphql-tools, thanks @Sceat

--- example/koa.js
+++ example/koa.js
@@ -1,39 +1,31 @@
 import Koa from "koa"
 import { readFileSync } from "fs"
 import { join, dirname } from "path"
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from "url"
 import bodyParser from "koa-bodyparser"
-import tools from 'graphql-tools'
-const { makeExecutableSchema } = tools
+import graphql from "graphql"
+import graphqlHTTP from "../src/koa"
 
-import graphql from "../src/koa"
+const { buildSchema } = graphql
 
 const dir = dirname(fileURLToPath(import.meta.url))
 
 const app = new Koa()
 	.use(bodyParser())
-	.use(graphql({
-		schema: makeExecutableSchema({
-			typeDefs: readFileSync(join(dir, "schema.gql"), "utf-8"),
-			resolvers: {
-				Query: {
-					hello(root, { name }) {
-						return `Hello ${name} !`
-					}
-				},
-				Subscription: {
-					onMessage: {
-						async *subscribe() {
-							while (true) {
-								console.log("lol")
-								await new Promise(resolve => setTimeout(resolve, 1000))
-								yield "Hello"
-							}
-						}
-					}
+	.use(graphqlHTTP({
+		schema: buildSchema(readFileSync(join(dir, "schema.gql"), "utf-8")),
+		rootValue: {
+			hello({ name }) {
+				return `Hello ${name} !`
+			},
+			async *onMessage() {
+				while (true) {
+					console.log("lol")
+					await new Promise(resolve => setTimeout(resolve, 1000))
+					yield "Hello"
 				}
 			}
-		})
+		}
 	}))
 
 const server = app.listen(3000, () => console.log("Listening on ", server.address()))
