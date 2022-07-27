1d41eeaddaaa8487a03df6ad0e529a5ccf3310ae
Sceat <fetch@sceat.xyz>
2022-07-27 15:46:17 +0300

    feat: add lambda support

---
+++ src/lambda.js
@@ -0,0 +1,16 @@
+import base from './base.js'
+
+const Lambda = (
+  { body: { query, variables, operationName } },
+  context,
+  reply
+) => ({
+  query,
+  variableValues: variables,
+  operationName,
+  reply: ({ type = 'application/json', ...body }) =>
+    reply(null, { statusCode: 200, body: JSON.stringify(body) }),
+})
+
+export default base(Lambda)
+export { k_field } from './base.js'
