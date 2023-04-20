c9f51e4deecfc2534ee1ca593c438b3e6c82f6ba
Sceat <fetch@sceat.xyz>
2023-04-20 16:10:09 +0400

    fix: use string body for lambda

--- src/lambda.js
+++ src/lambda.js
@@ -1,16 +1,15 @@
 import base from './base.js'
 
-const Lambda = (
-  { body: { query, variables, operationName } = {} },
-  context,
-  reply
-) => ({
-  query,
-  variableValues: variables,
-  operationName,
-  reply: ({ type = 'application/json', ...body }) =>
-    reply(null, { statusCode: 200, body: JSON.stringify(body) }),
-})
+const Lambda = ({ body: raw_body }, context, reply) => {
+  const { query, operationName, variables } = JSON.parse(raw_body)
+  return {
+    query,
+    variableValues: variables,
+    operationName,
+    reply: ({ type = 'application/json', ...body }) =>
+      reply(null, { statusCode: 200, body: JSON.stringify(body) }),
+  }
+}
 
 export default base(Lambda)
 export { k_field } from './base.js'
